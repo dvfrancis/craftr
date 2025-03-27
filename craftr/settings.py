41755cf65b51e23f5bd87feee51a4f8f939002c2
dvfrancis <github@dominicfrancis.co.uk>
2025-03-27 15:18:56 +0000

    test: test custom error 500 page

--- craftr/settings.py
+++ craftr/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'craftr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'wrong',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
