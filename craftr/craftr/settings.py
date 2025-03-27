4b6175ec36568016df0204c1944b7ad43ca67628
dvfrancis <github@dominicfrancis.co.uk>
2025-03-27 11:53:11 +0000

    refactor: update settings.py

--- craftr/craftr/settings.py
+++ craftr/craftr/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = ['django-insecure-o-o(vp&gu1qzx87h)ox$6isdn+7)8zyp5izfw1r+^^=-4v9+%=']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
