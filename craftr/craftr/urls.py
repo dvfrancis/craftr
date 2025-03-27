333016d3f672facf49606767a6d31ab566de2879
dvfrancis <github@dominicfrancis.co.uk>
2025-03-27 10:44:50 +0000

    feat: add home page app

--- craftr/craftr/urls.py
+++ craftr/craftr/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from craftr.views import home
 
 urlpatterns = [
+    path('', craftr.views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
