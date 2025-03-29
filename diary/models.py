0fb1c34bd4ed68d60d00087c1ae271f14c5b5e56
dvfrancis <github@dominicfrancis.co.uk>
2025-03-29 12:00:53 +0000

    refactor: update diary model

--- diary/models.py
+++ diary/models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 
 class EventDay(models.Model):
-    event_date = models.DateField()
+    class_date = models.DateField()
     event_title = models.CharField(max_length=100, unique=True)
 
     # Enforce unique event titles regardless of case
@@ -19,17 +19,17 @@ class EventDay(models.Model):
 
     def clean(self):
         # Require all fields to be completed
-        if not self.event_date or not self.event_title:
+        if not self.class_date or not self.event_title:
             raise ValidationError("All fields must be completed")
 
     def __str__(self):
         return (
-            f"{self.event_date}")
+            f"{self.class_date}")
 
 
 @admin.register(EventDay)
 class EventDayAdmin(admin.ModelAdmin):
-    list_display = ('event_date', 'event_title')
-    list_filter = ('event_date', 'event_title')
-    search_fields = ('event_date', 'event_title')
-    ordering = ['event_date']
+    list_display = ('class_date', 'event_title')
+    list_filter = ('class_date', 'event_title')
+    search_fields = ('class_date', 'event_title')
+    ordering = ['class_date']
