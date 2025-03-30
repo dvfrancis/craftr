9bd413a9e152e86106b1f28ccec1c9b9f24f38d1
dvfrancis <github@dominicfrancis.co.uk>
2025-03-30 15:17:06 +0100

    feat: enabled contact form with email functionality

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('message', 'first_name', 'last_name', 'email')
+    list_filter = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name', 'email', 'message')
+    ordering = ['first_name', 'last_name', 'email']
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from .forms import ContactForm
+import os
 
 
 def contact_page(request):
-    return render(request, 'contact/contact.html')
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save to database
+            send_mail(
+                subject="Craftr Contact Form Submission",
+                message=(
+                    f"Name: {form.cleaned_data['first_name']} "
+                    f"{form.cleaned_data['last_name']}\n"
+                    f"Email: {form.cleaned_data['email']}\n"
+                    f"Message: {form.cleaned_data['message']}"
+                ),
+                from_email=form.cleaned_data['email'],
+                recipient_list=[os.getenv('EMAIL_USER')],
+            )
+            return redirect('home')  # Redirect after submission
+    else:
+        form = ContactForm()
+    return render(request, "contact/contact.html", {"form": form})
+
