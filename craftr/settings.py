3d9423fab5d0ea81d0381b06df691ac37bd0c61a
dvfrancis <github@dominicfrancis.co.uk>
2025-04-02 11:09:43 +0100

    refactor: change debug status to false

--- craftr/settings.py
+++ craftr/settings.py
@@ -39,7 +39,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
