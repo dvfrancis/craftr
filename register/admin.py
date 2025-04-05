dafa7309f6ffcf67edf8e12a7df839c79de6f970
dvfrancis <github@dominicfrancis.co.uk>
2025-04-05 16:36:07 +0100

    refactor: update register admin.py

--- register/admin.py
+++ register/admin.py
@@ -4,7 +4,7 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'location', 'experience', 'photograph')
+    list_display = ('user', 'location', 'experience')
     list_filter = ('user', 'location', 'experience')
     search_fields = ('user__username', 'location', 'experience')
     ordering = ['user__username', 'location', 'experience']
