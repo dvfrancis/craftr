cb6d4bc9567547634933b70c3481cf98d700bfc7
dvfrancis <github@dominicfrancis.co.uk>
2025-04-14 12:02:37 +0100

    refactor: add placeholder text to contact form

--- contact/forms.py
+++ contact/forms.py
@@ -6,3 +6,17 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['first_name', 'last_name', 'email', 'message']
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={'placeholder': 'Enter your first name'}
+            ),
+            'last_name': forms.TextInput(
+                attrs={'placeholder': 'Enter your last name'}
+            ),
+            'email': forms.TextInput(
+                attrs={'placeholder': 'Enter your email address'}
+            ),
+            'message': forms.TextInput(
+                attrs={'placeholder': 'Enter your message'}
+            ),
+        }
