96f020c13fa37c46a27389329ab3f5250e43a3c4
dvfrancis <github@dominicfrancis.co.uk>
2025-04-15 12:06:40 +0100

    style: adjust navbar styles

--- craftr/static/craftr/base.js
+++ craftr/static/craftr/base.js
@@ -1,5 +1,3 @@
-/* Bootstrap toast notifications */
-
 document.addEventListener("DOMContentLoaded", function () {
   var toastEl = document.getElementById("toastMessage");
   var toast = new bootstrap.Toast(toastEl);
