4c3e19cee131c16cca0529a9f38b45308d5f54fc
Dominic Francis <github@dominicfrancis.co.uk>
2025-10-13 15:11:30 +0100

    refactor: update settings.py

--- craftr/settings.py
+++ craftr/settings.py
@@ -148,21 +148,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'account/static'),
-    os.path.join(BASE_DIR, 'contact/static'),
-    os.path.join(BASE_DIR, 'craftr/static'),
-    os.path.join(BASE_DIR, 'details/static'),
-    os.path.join(BASE_DIR, 'diary/static'),
-    os.path.join(BASE_DIR, 'faq/static'),
-    os.path.join(BASE_DIR, 'home/static'),
-    os.path.join(BASE_DIR, 'login/static'),
-    os.path.join(BASE_DIR, 'register/static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
